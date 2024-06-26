a4e16d6cb5990a0da9c4a0ab65cc4b1331f13794
mlzepplin <rishabh.gupta.1606@gmail.com>
2024-06-26 15:32:59 -0700

    add basic form html and handler

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'hello test'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return render_template('index.html', name=name, email=email)
+    else:
+        return render_template('index.html')
+
+
+@app.route('/creds/', methods=['GET', 'POST'])
+def creds():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        return render_template('index.html', name=name, email=email)
+    else:
+        return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
